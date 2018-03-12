d6e03dce9b826efaeaa7bcbbea7a0b2719575c99
Prateek <prateekpandey9928@gmail.com>
2018-03-11 18:00:09 -0700

    Data Preprocessing

---
+++ Part 1 - Data Preprocessing/temp.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print('hello world!')
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Data.csv')
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,3].values
+
+
+from sklearn.preprocessing import Imputer
+imputer=Imputer(missing_values='NaN',strategy='mean',axis=0)
+imputer=imputer.fit(X[:,1:3])
+X[:,1:3]=imputer.transform(X[:,1:3])
+
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder=LabelEncoder()
+labelencoder=labelencoder.fit(X[:,0])
+X[:,0]=labelencoder.transform(X[:,0])
+onehotencoder=OneHotEncoder(categorical_features=[0])
+X=onehotencoder.fit_transform(X).toarray()
+
+labelencoder_y=LabelEncoder()
+y=labelencoder.fit_transform(y)
